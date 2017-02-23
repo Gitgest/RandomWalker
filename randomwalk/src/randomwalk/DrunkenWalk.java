87162fb405e69679b8bb072e7ba7f4640d3d1ff8
Rasmus laptop <Rasmus laptop@192.168.0.20>
2017-02-23 17:16:21 +0100

    First git push of project Second push, added moreSteps() and inBounds() Modified inBounds with negative boundary values. added walk() method, tested on walker object, success! Added DrunkenWalk class

---
+++ randomwalk/src/randomwalk/DrunkenWalk.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package randomwalk;
+
+/**
+ *
+ * @author Rasmus laptop
+ */
+public class DrunkenWalk {
+    
+}
